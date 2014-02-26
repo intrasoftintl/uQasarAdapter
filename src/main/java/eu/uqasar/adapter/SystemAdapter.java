74de092b4ce047007430178b78ba099b553dc50a
eleni <fwtopoulou@gmail.com>
2014-02-26 16:42:24 +0200

    add uQasarAdapter.jar

--- src/main/java/eu/uqasar/adapter/SystemAdapter.java
+++ src/main/java/eu/uqasar/adapter/SystemAdapter.java
@@ -2,11 +2,9 @@ package eu.uqasar.adapter;
 
 import eu.uqasar.adapter.exception.uQasarException;
 import eu.uqasar.adapter.model.BindedSystem;
-import eu.uqasar.adapter.model.Measurement;
 import eu.uqasar.adapter.model.User;
 import eu.uqasar.adapter.query.QueryExpression;
-
-import java.util.List;
+import org.codehaus.jettison.json.JSONArray;
 
 /**
  * uQasar System Adapter Interface
@@ -16,8 +14,8 @@ public interface SystemAdapter {
     /*
      *   The query method accepts a System-specific query-Expression and Returns a UQasar-specific Result
      */
-   public List<Measurement> query(BindedSystem bindedSystem, User user, QueryExpression queryExpression) throws uQasarException;
+   public JSONArray query(BindedSystem bindedSystem, User user, QueryExpression queryExpression) throws uQasarException;
 
-   public List<Measurement> query(String bindedSystemURL, String credentials, String queryExpression) throws uQasarException;
+   public JSONArray query(String bindedSystemURL, String credentials, String queryExpression) throws uQasarException;
 
     }//EoClass SystemAdapter
