10037f3dfac5d709967f793443a4d4a9947cc764
Waleri Enns <wen@contact.de>
2014-03-06 12:46:40 +0100

    Docstrings for the SystemAdapter interface

--- src/main/java/eu/uqasar/adapter/SystemAdapter.java
+++ src/main/java/eu/uqasar/adapter/SystemAdapter.java
@@ -12,12 +12,29 @@ import java.util.List;
  * uQasar System Adapter Interface
  */
 public interface SystemAdapter {
-
-    /*
-     *   The query method accepts a System-specific query-Expression and Returns a UQasar-specific Result
-     */
-   public  List<Measurement> query(BindedSystem bindedSystem, User user, QueryExpression queryExpression) throws uQasarException;
-
-   public List<Measurement> query(String bindedSystemURL, String credentials, String queryExpression) throws uQasarException;
-
-    }//EoClass SystemAdapter
+  /**
+   * The query method accepts a System-specific query-Expression and
+   * Returns a UQasar-specific Result
+   *
+   * @param bindedSystem The adapted system
+   * @param user The credentials needed to access the system
+   * @param queryExpression The (uQuasar-specific) query to send to the system.
+   */
+  public  List<Measurement> query(BindedSystem bindedSystem, User user,
+                                  QueryExpression queryExpression) throws uQasarException;
+  
+  /**
+   * The query method accepts a System-specific query-Expression and
+   * returns a UQasar-specific Result
+   *
+   * @param bindedSystem The URL of the adapted system
+   * @param credentials The credentials needed to access the adapted system.
+            The syntax is adapter specific.
+   * @param queryExpression The query to send to the adapted system. The syntax is:
+   *        <uquasar_metric_name> [, <<uquasar_metric_name>, ...]
+   *
+   * @return List containing Measurement objects. Measurement.measurement is JSON-Formatted data,
+   *         the structure is adapter specific.
+   */
+  public List<Measurement> query(String bindedSystemURL, String credentials, String queryExpression) throws uQasarException;
+}//EoClass SystemAdapter
