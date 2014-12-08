1e7107160a61304e96e9a63f7efa802d1f38577c
Waleri Enns <waleri.enns@gmail.com>
2014-12-08 14:50:57 +0100

    UQ-194 (Update the SonarQube adapter) Extend the metrics enum by added SonarQube metrics

--- src/main/java/eu/uqasar/adapter/model/uQasarMetric.java
+++ src/main/java/eu/uqasar/adapter/model/uQasarMetric.java
@@ -8,15 +8,32 @@ public enum uQasarMetric {
     UNRESOLVED_ISSUES_PER_PROJECT,
     UNRESOLVED_BUG_ISSUES_PER_PROJECT,
     UNRESOLVED_TASK_ISSUES_PER_PROJECT,
-    
+
     //  Source code metrics
-    NCLOC,                         // lines of code (NC=non comment)
-    STATEMENTS,                    // number of statements
-    DUPLICATED_LINES,              // number of duplicated lines
-    DUPLICATED_LINES_DENSITY,      // the density of duplicated lines (number in [0..100])
-    COMPLEXITY,                    // cyclomatic complexity of the project
-    UT_COVERAGE,                   // code coverage by unit tests
-    AT_COVERAGE,                   // code coverage by acceptance tests
-    TEST_SUCCESS_DENSITY           // ratio of successfully executed tests to the overall number of tests
+    NCLOC,                    // lines of code (NC=non comment)
+    LINES,                    // lines (i.e. all lines)
+    COMMENT_LINES,            // number of comment lines
+    STATEMENTS,               // number of statements
+    CLASSES,                  // number of classes
+    FILES,                    // number of files
+    DIRECTORIES,              // number of directories
+    FUNCTIONS,                // number of functions
+    COMMENT_LINES_DENSITY,    // the density of comment lines (number in [0..100])
+
+    DUPLICATED_LINES,         // number of duplicated lines
+    DUPLICATED_LINES_DENSITY, // the density of duplicated lines (number in [0..100])
+    COMPLEXITY,               // cyclomatic complexity of the project
+    FUNCTION_COMPLEXITY,      // the average cyclomatic complexity of a function
+    FILE_COMPLEXITY,          // the average cyclomatic complexity of a source code file
+    CLASS_COMPLEXITY,         // the average cyclomatic complexity of a class
+    UT_COVERAGE,              // code coverage by unit tests
+    AT_COVERAGE,              // code coverage by acceptance tests
+    OVERALL_COVERAGE,         // code coverage by both, unit and acceptance tests
+    PACKAGE_TANGLES,          // Number of file dependencies to cut in order to remove all cycles between directories.
+    PACKAGE_TANGLE_INDEX,     // Level of directory interdependency. Best value (0%) means that there is no cycle and worst value (100%) means that directories are really tangled.
+    TEST_SUCCESS_DENSITY,     // ratio of successfully executed tests to the overall number of te
+    TEST_FAILURES,            // number of failed tests (failure = test could be run and is failed)
+    TEST_ERRORS,              // number of errored tests (error = test could not be run)
+    TESTS                     // number of tests
 }
 
